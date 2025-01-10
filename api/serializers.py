8fed9374a56c0e094a48744cabc5e2e506e2e6c8
j1ere <jeremywambua106@gmail.com>
2025-01-10 15:44:15 +0300

    user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, validators=[validate_password])#ensures password is not exposed in API responses and is strong
+    username = serializers.CharField(max_length=100, required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+    email = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        #harsh the password before saving the user
-        if User.objects.filter(username=validated_data['username']):
-            raise serializers.ValidationError({'error':'username already exists'})
+        #hash the password before saving the user
         user = User(username=validated_data['username'], email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
