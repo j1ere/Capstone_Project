d18a556c59f92497628b3a1fec2298b42c13c246
j1ere <jeremywambua106@gmail.com>
2025-01-11 12:35:09 +0300

    pagination setup, pagenumberpagination

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -134,5 +134,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # Ensure the user is authenticated
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, #number of items per page
 }
