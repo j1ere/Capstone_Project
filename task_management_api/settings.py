c83419e1cba82e61494001c9e25e3df1d2fe9245
j1ere <jeremywambua106@gmail.com>
2025-01-12 07:38:57 +0300

    allowed hosts

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-@#!hijgl%4ecsjje^hf$%$vtn87%bv!*ap+^_59n-qc3fs@65^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
